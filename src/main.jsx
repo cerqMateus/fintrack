f577b6af1c3f8d1c5d32485659d605c69c642bd4
Mateus Cerqueira <mateus.cerq7@outlook.com>
2025-03-24 13:13:55 -0300

    feat: add pages setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+
+import HomePage from "./pages/home";
+import LoginPage from "./pages/login";
+import PageNotFound from "./pages/PageNotFound";
+import SignUpPage from "./pages/signup";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
